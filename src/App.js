3f7df35a5d0668c75ace9eb900fe3f0a2062b9de
Maverick <sc20ml@leeds.ac.uk>
2024-01-07 17:45:20 +0000

    Home page complete with Cards and footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
+import Home from './components/pages/Home';
 
 const App = () => {
     return (
         <div className= 'App'>
             <Router>
                 <Navbar/>
-                <Routes path = '/' exact />
+                <Routes>
+                    {/* <Route path = '/' exact component = {Home} /> */}
+                    <Route path='/' exact element={ <Home />} />
+                </Routes>
             </Router>
-            
         </div>
     )
 }
